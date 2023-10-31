19b2c16a6dd3a4e1fd4ae1c9061818fac220fa03
Carlos D'Santiago <carlosdavidsantiago4@gmail.com>
2023-10-30 21:59:58 -0400

    .

--- src/js/functions/netlify.proxy.funtions.js
+++ src/js/functions/netlify.proxy.funtions.js
@@ -3,27 +3,19 @@ const { Response } = require('@netlify/functions');
 
 exports.handler = async (event) => {
 	try {
+		// Realiza la solicitud a la API de IPStack
 		const apiResponse = await fetch(
 			`http://api.ipstack.com/${event.queryStringParameters.ip}?access_key=061de896653a2cbfc3a0d29864d4ea9a`
 		);
 		const apiData = await apiResponse.json();
 
-		const { region_code, latitude, longitude, zip } = apiData;
-
-		const response = new Response(
-			JSON.stringify({
-				regionCode: region_code,
-				latitude,
-				longitude,
-				postal: zip,
-			}),
-			{
-				headers: {
-					'Access-Control-Allow-Origin': '*', // Configura esto según tus necesidades
-					'Access-Control-Allow-Methods': 'GET',
-				},
-			}
-		);
+		// Configura los encabezados CORS para permitir todas las solicitudes
+		const response = new Response(JSON.stringify(apiData), {
+			headers: {
+				'Access-Control-Allow-Origin': '*', // Esto permite todas las solicitudes
+				'Access-Control-Allow-Methods': 'GET',
+			},
+		});
 
 		return response;
 	} catch (error) {
